45c6e7219d8c91b4a05af9490d7549fe08c0d2c0
kaarla <sgakarla@ciencias.unam.mx>
2021-05-16 17:54:32 -0500

    general version

--- session6/problem2.go
+++ session6/problem2.go
@@ -2,28 +2,52 @@ package main
 
 import(
 	"fmt"
+	"sort"
 )
 
 func main() {
 	fmt.Println("For $6")
-	calculateCoins(6)
+	calculateGeneralCoins(6, []int{5, 2, 1,10})
 	fmt.Println("For $8")
-	calculateCoins(8)
+	calculateGeneralCoins(8, []int{2, 1, 10, 5})
+	fmt.Println("For $10")
+	calculateCoins(10)
+	fmt.Println("For $5")
+	calculateCoins(5)
+	fmt.Println("For $2")
+	calculateCoins(2)
+	fmt.Println("For $1")
+	calculateCoins(1)
 }
 
 func calculateCoins(n int){
 	ten, five, two := 0, 0, 0
-	if n > 10 {
+	if n >= 10 {
 		ten, n = int(n / 10), n % 10
 		fmt.Printf("%d coins of 10\n", ten)
 	}
-	if n > 5 {
+	if n >= 5 {
 		five, n = int(n / 5), n % 5
 		fmt.Printf("%d coins of 5\n", five)
 	}
-	if n > 2 {
+	if n >= 2 {
 		two, n = int(n / 2), n % 2
 		fmt.Printf("%d coins of 2\n", two)
 	}
-	fmt.Printf("%d coins of 1\n", n)
+	if n >= 1 {
+		fmt.Printf("%d coins of 1\n", n)
+	}
 }
+
+
+func calculateGeneralCoins(n int, available []int) {
+	sort.Sort(sort.Reverse(sort.IntSlice(available)))
+	coins := 0
+	for _, denom := range available {
+		if n >= denom {
+			coins, n = int(n / denom), n % denom
+			fmt.Printf("%d coins of %d\n", coins, denom)
+		}
+	}
+}
+
