616ce296bd5002ed7c50168d0add12a73f1f58b3
kaarla <sgakarla@ciencias.unam.mx>
2021-05-16 18:50:44 -0500

    getCombinations

--- session6/problem1.go
+++ session6/problem1.go
@@ -6,16 +6,11 @@ import(
 
 func main() {
 	//var arr3 [4]int //no sé por qué esto no jala
-	arr3 := []int{0,0,0,0}
-	fmt.Println("comb de 3:", getCombinations(3, arr3))
-	arr4 := []int{0,0,0,0,0}
-	fmt.Println("comb de 4:", getCombinations(4, arr4))
-	arr5 := []int{0,0,0,0,0,0}
-	fmt.Println("comb de 4:", getCombinations(5, arr5))
-
+	fmt.Println(getCombinations(3))
+	fmt.Println(getCombinations(4))
 }
 
-func getCombinations(n int, memo []int) int{
+func getCombinationsQuantity(n int, memo []int) int{
 	if n == 1 {
 		return 2
 	}
@@ -23,8 +18,27 @@ func getCombinations(n int, memo []int) int{
 		return 3
 	}
 	if memo[n] == 0 {
-		memo[n] = getCombinations(n - 1, memo) +
-			getCombinations(n -2, memo)
+		memo[n] = getCombinationsQuantity(n - 1, memo) +
+			getCombinationsQuantity(n -2, memo)
 	}
 	return memo[n]
+}
+
+func getCombinations(n int) []string{
+	if n == 1 {
+		return []string{"0", "1"}
+	}
+	if n == 2 {
+		return []string{"00", "01", "10"}
+	}
+	
+	var combinations []string
+	for _, comb := range getCombinations(n - 2) {
+		combinations = append(combinations, "10" + comb)
+		combinations = append(combinations, "00" + comb)
+		if comb[0] == '0' {
+			combinations = append(combinations, "01" + comb)
+		}
+	}
+	return combinations
 }
