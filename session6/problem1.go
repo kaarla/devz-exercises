1d923082f0ea5686e7fb1f824a7b76964d756047
kaarla <sgakarla@ciencias.unam.mx>
2021-05-16 16:36:48 -0500

    session6 problem1

---
+++ session6/problem1.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hola mundo")
+	//var arr3 [4]int //no sé por qué esto no jala
+	arr3 := []int{0,0,0,0}
+	fmt.Println("comb de 3:", getCombinations(3, arr3))
+	arr4 := []int{0,0,0,0,0}
+	fmt.Println("comb de 4:", getCombinations(4, arr4))
+	arr5 := []int{0,0,0,0,0,0}
+	fmt.Println("comb de 4:", getCombinations(5, arr5))
+
+}
+
+func getCombinations(n int, memo []int) int{
+	if n == 1 {
+		return 2
+	}
+	if n == 2 {
+		return 3
+	}
+	if memo[n] == 0 {
+		memo[n] = getCombinations(n - 1, memo) +
+			getCombinations(n -2, memo)
+	}
+	return memo[n]
+}
